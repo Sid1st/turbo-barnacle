8d7ca4bc5b1ef131ec6649e6470a891fee313ba7
Sid1st <97543215+Sid1st@users.noreply.github.com>
2024-07-13 01:42:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -101,9 +101,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://turbo-barnacle-8ec483f3770c.herokuapp.com/"
+        URL = ""
     else:
-        URL = "https://turbo-barnacle-8ec483f3770c.herokuapp.com/"
+        URL = ""
 
 
 
